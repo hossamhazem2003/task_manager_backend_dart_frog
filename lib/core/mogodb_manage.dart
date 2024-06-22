8414dc6b5f0be9bea2ec7fe3475a639f1c4362ed
Hossam Hazem <hossam.hazem.1903@gmail.com>
2024-06-22 03:18:26 +0300

    hide database link

--- lib/core/mogodb_manage.dart
+++ lib/core/mogodb_manage.dart
@@ -3,7 +3,7 @@ import 'package:mongo_dart/mongo_dart.dart';
 class DataBase {
   static Future<Db> createDb() async {
     final db = await Db.create(
-      'mongodb+srv://hossamhazem:12345678910@backendtest1cluster.zodjjxa.mongodb.net/?retryWrites=true&w=majority&appName=backEndTest1Cluster',
+      'mongodb+srv://<username>:<password>@backendtest1cluster.zodjjxa.mongodb.net/?retryWrites=true&w=majority&appName=<cluster>',
     );
 
     if (!db.isConnected) {
